09fd9a8ca5f6cabae7eadbdd026b52b59056b60c
kamilMrox <kamilmrox@gmail.com>
2025-11-15 22:27:12 +0100

    fixed small mistake

--- main.py
+++ main.py
@@ -9,10 +9,7 @@ def main():
 def get_book_text(path):
 	with open(path) as f:
 		return f.read()
-
-book_path = "books/frankenstein.txt"
-text = get_book_text(book_path)
-
+	
 def get_num_words(book):
 	number_of_words = len(book.split())
 	return number_of_words
