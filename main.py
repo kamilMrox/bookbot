82c79e0acb73c956d19d53c29c93cac0961b7822
kamilMrox <kamilmrox@gmail.com>
2025-11-15 22:36:54 +0100

    small refactor

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+from stats import get_num_words
 
 def main():
 	book_path = "books/frankenstein.txt"
@@ -10,9 +11,4 @@ def get_book_text(path):
 	with open(path) as f:
 		return f.read()
 	
-def get_num_words(book):
-	number_of_words = len(book.split())
-	return number_of_words
-
-
 main()
---
+++ stats.py
@@ -0,0 +1,3 @@
+def get_num_words(book):
+	number_of_words = len(book.split())
+	return number_of_words
