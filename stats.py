63b6df899a3fb7c61335119450fffa0a04c21b7e
kamilMrox <kamilmrox@gmail.com>
2025-11-16 00:09:49 +0100

    printing a report feature

--- stats.py
+++ stats.py
@@ -1,13 +1,23 @@
-def get_num_words(book):
-	number_of_words = len(book.split())
+char = {}
+def get_num_words(text):
+	number_of_words = len(text.split())
 	return number_of_words
 
-def get_char_count(book):
-    char = {}
-    for ch in book.lower():
+def get_chars_dict(text):
+    for ch in text.lower():
           if ch not in char:
                 char[ch] = 0
           if ch in char:
                 char[ch] += 1
-
     return char
+
+def sort_on(d):
+      return d["num"]
+
+def chars_dict_to_sorted_list(num_chars_dict):
+      sorted_list = []
+      for ch in num_chars_dict:
+            sorted_list.append({"char": ch, "num":num_chars_dict[ch]})
+      sorted_list.sort(reverse=True, key=sort_on)
+      return sorted_list
+
