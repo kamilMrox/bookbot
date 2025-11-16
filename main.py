b2ae0cc34b821969d20277ca0c817837c4025d2b
kamilMrox <kamilmrox@gmail.com>
2025-11-16 22:50:26 +0100

    finished project last commit

--- main.py
+++ main.py
@@ -1,17 +1,22 @@
+import sys
 from stats import (
     get_num_words,
     chars_dict_to_sorted_list,
     get_chars_dict,
 )
 
+if len(sys.argv) != 2:
+    print("Usage: python3 main.py <path_to_book>")
+    sys.exit(1)
+else:
+    book_path = sys.argv[1]
+
 def main():
-    book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     num_words = get_num_words(text)
     chars_dict = get_chars_dict(text)
     chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
     print_report(book_path, num_words, chars_sorted_list)
-
 
 def get_book_text(path):
     with open(path) as f:
